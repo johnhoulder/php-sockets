6e0e60d15c6c9d295057488576a2d15fd42aad4e
GtoXic <rocky4126@hotmail.co.uk>
2011-05-31 13:30:23 +0100

    Re-indented lines

--- client.php
+++ client.php
@@ -15,124 +15,133 @@ socket_listen($sock);
 
 function broadcast($string, $except = -1)
 {
-global $max_clients, $client;
-echo $string . PHP_EOL;
-for ($i = 0; $i < $max_clients; $i++)
-{
-if ($client[$i]['sock'] && $client[$i]['user']['name'] && $i !== $except)
-{
-socket_write($client[$i]['sock'], $string . "\r\n");
-}
-}
+	global $max_clients, $client;
+	echo $string . PHP_EOL;
+	for ($i = 0; $i < $max_clients; $i++)
+	{
+		if ($client[$i]['sock'] && $client[$i]['user']['name'] && $i !== $except)
+		{
+			socket_write($client[$i]['sock'], $string . "\r\n");
+		}
+	}
 }
 
 while (true)
 {
-$read = array();
-$read[0] = $sock;
-for ($i = 0; $i < $max_clients; $i++)
-{
-if ($client[$i]['sock'] != null)
-{
-$read[$i + 1] = $client[$i]['sock'] ;
-}
-}
-
-$ready = socket_select($read, $write = null, $except = null, $tv_sec = null);
-if (in_array($sock, $read))
-{
-for ($i = 0; $i < $max_clients; $i++)
-{
-if ($client[$i]['sock'] == null)
-{
-$client[$i]['sock'] = socket_accept($sock);
-socket_write($client[$i]['sock'], "Welcome. Please use \"NICK yournick\" to get started.\r\n");
-echo 'Accepted socket.' . PHP_EOL;
-break;
-}
-else if ($i == $max_clients - 1)
-{
-echo 'Too many clients.' . PHP_EOL;
-}
-}
-if (--$ready <= 0)
-{
-continue;
-}
-}
-
-for ($i = 0; $i < $max_clients; $i++)
-{
-if (in_array($client[$i]['sock'] , $read))
-{
-$input = socket_read($client[$i]['sock'] , 1024, PHP_NORMAL_READ);
-if ($input == null)
-{
-broadcast('QUIT ' . $client[$i]['user']['name']);
-$fp = fopen("./server.users", "r");
-$users = fgets($fp);
-fclose($fp);
-$fp2 = fopen("./server.users", "w");
-$users = explode(" ", $users);
-foreach($users as $user){
-	if($user != $client[$i]['user']['name']){
-		fwrite($user, $fp2." ");
+	$read = array();
+	$read[0] = $sock;
+	for ($i = 0; $i < $max_clients; $i++)
+	{
+		if ($client[$i]['sock'] != null)
+		{
+			$read[$i + 1] = $client[$i]['sock'] ;
+		}
 	}
-}
-fclose($fp2);
-unset($client[$i]);
-echo 'Unset socket.' . PHP_EOL;
-}
-$input = trim($input);
-if ($input == 'exit')
-{
-broadcast('QUIT ' . $client[$i]['quit']['name']);
-socket_close($client[$i]['sock']);
-unset($client[$i]);
-echo 'Closed socket.' . PHP_EOL;
-}
-else if ($input !== '')
-{
-$cmd = explode(' ', $input);
 
-if (!isset($client[$i]['user']['name']))
-{
-if (strtoupper($cmd[0]) == 'NICK')
-{
-$taken = false;
-foreach ($client as $clien)
-{
-if ($clien['user']['name'] == $cmd[1])
-{
-$taken = true;
-break;
-}
-}
+	$ready = socket_select($read, $write = null, $except = null, $tv_sec = null);
+	if (in_array($sock, $read))
+	{
+		for ($i = 0; $i < $max_clients; $i++)
+		{
+			if ($client[$i]['sock'] == null)
+			{
+				$client[$i]['sock'] = socket_accept($sock);
+				socket_write($client[$i]['sock'], "Welcome. Please use \"NICK yournick\" to get started.\r\n");
+				echo 'Accepted socket.' . PHP_EOL;
+				break;
+			}
+			else if ($i == $max_clients - 1)
+			{
+				echo 'Too many clients.' . PHP_EOL;
+			}
+		}
+		if (--$ready <= 0)
+		{
+			continue;
+		}
+	}
 
-if ($taken)
-{
-socket_write($client[$i]['sock'], "ERROR 002 That name is already taken. Try again.\r\n");
-}
-else
-{
-$client[$i]['user']['name'] = $cmd[1];
-broadcast('JOIN ' . $cmd[1]);
-$fp = fopen("./server.users", "a+");
-fwrite($client[$i]['user']['name'], $fp);
-fclose($fp);
-}
-}
-else
-{
-socket_write($client[$i]['sock'], "ERROR 001 No NICK specified. Please specify your NICK using \"NICK yournick\".\r\n");
-}
-continue;
-}
+	for ($i = 0; $i < $max_clients; $i++)
+	{
+		if (in_array($client[$i]['sock'] , $read))
+		{
+			$input = socket_read($client[$i]['sock'] , 1024, PHP_NORMAL_READ);
+			if ($input == null)
+			{
+				broadcast('QUIT ' . $client[$i]['user']['name']);
+				$fp = fopen("./server.users", "r");
+				$users = fgets($fp);
+				fclose($fp);
+				$fp2 = fopen("./server.users", "w");
+				$users = explode(" ", $users);
+				foreach($users as $user){
+					if($user != $client[$i]['user']['name']){
+						fwrite($user, $fp2." ");
+					}
+				}
+				fclose($fp2);
+				unset($client[$i]);
+				echo 'Unset socket.' . PHP_EOL;
+			}
+			$input = trim($input);
+			if ($input == 'exit')
+			{
+				broadcast('QUIT ' . $client[$i]['quit']['name']);
+								$fp = fopen("./server.users", "r");
+				$users = fgets($fp);
+				fclose($fp);
+				$fp2 = fopen("./server.users", "w");
+				$users = explode(" ", $users);
+				foreach($users as $user){
+					if($user != $client[$i]['user']['name']){
+						fwrite($user, $fp2." ");
+					}
+				}
+				socket_close($client[$i]['sock']);
+				unset($client[$i]);
+				echo 'Closed socket.' . PHP_EOL;
+			}
+			else if ($input !== '')
+			{
+				$cmd = explode(' ', $input);
+				if (!isset($client[$i]['user']['name']))
+				{
+					if (strtoupper($cmd[0]) == 'NICK')
+					{
+						$taken = false;
+						foreach ($client as $clien)
+						{
+							if ($clien['user']['name'] == $cmd[1])
+							{
+								$taken = true;
+								break;
+							}
+						}
 
-include("./switch.php");
-}
-}
-}
+						if ($taken)
+						{
+							socket_write($client[$i]['sock'], "ERROR 002 That name is already taken. Try again.\r\n");
+						}
+						else
+						{
+							$client[$i]['user']['name'] = $cmd[1];
+							broadcast('JOIN ' . $cmd[1]);
+							$fp = fopen("./server.users", "a+");
+							fwrite($client[$i]['user']['name'], $fp);
+							fclose($fp);
+						}
+					}
+					else
+					{
+						socket_write($client[$i]['sock'], "ERROR 001 No NICK specified. Please specify your NICK using \"NICK yournick\".\r\n");
+					}
+					continue;
+				}
+
+			include("./switch.php");
+			}
+		}
+	}
 }
 
 socket_close($sock);
--- switch.php
+++ switch.php
@@ -1,40 +1,40 @@
 <?php
 switch (strtoupper($cmd[0]))
 {
-case 'ACT':
-preg_match('/^ACT (.+)$/', $input, $matches);
-broadcast($client[$i]['user']['name'] . ' ACT ' . $matches[1], $i);
-break;
+	case 'ACT':
+		preg_match('/^ACT (.+)$/', $input, $matches);
+		broadcast($client[$i]['user']['name'] . ' ACT ' . $matches[1], $i);
+		break;
 
-case 'MSG':
-preg_match('/^MSG (.+)$/', $input, $matches);
-broadcast($client[$i]['user']['name'] . ' MSG ' . $matches[1], $i);
-break;
+	case 'MSG':
+		preg_match('/^MSG (.+)$/', $input, $matches);
+		broadcast($client[$i]['user']['name'] . ' MSG ' . $matches[1], $i);
+		break;
 
-case 'NICK':
-$taken = false;
-foreach ($client as $clien)
-{
-if ($clien['user']['name'] == $cmd[1])
-{
-$taken = true;
-break;
-}
-}
+	case 'NICK':
+		$taken = false;
+		foreach ($client as $clien)
+		{
+			if ($clien['user']['name'] == $cmd[1])
+			{
+				$taken = true;
+				break;
+			}
+		}
 
-if ($taken)
-{
-socket_write($client[$i]['sock'], "ERROR 002 That name is already taken.\r\n");
-}
-else
-{
-broadcast($client[$i]['user']['name'] . ' NICK ' . $cmd[1]);
-$client[$i]['user']['name'] = $cmd[1];
-}
-break;
+		if ($taken)
+		{
+			socket_write($client[$i]['sock'], "ERROR 002 That name is already taken.\r\n");
+		}
+		else
+		{
+			broadcast($client[$i]['user']['name'] . ' NICK ' . $cmd[1]);
+			$client[$i]['user']['name'] = $cmd[1];
+		}
+		break;
 
-default:
-socket_write($client[$i]['sock'], 'Error 404: Command ' . strtoupper($cmd[0]) . " not found.\r\n");
-break;
+	default:
+		socket_write($client[$i]['sock'], 'Error 404: Command ' . strtoupper($cmd[0]) . " not found.\r\n");
+		break;
 }
 ?>
